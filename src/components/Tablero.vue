a00ff06bce759628078e4c623752c3b16fff04b9
Elian Bustamante <e.bustamante02@ufromail.cl>
2022-06-17 15:44:05 -0400

    Agregar componente para el tablero base

---
+++ src/components/Tablero.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="tablero" v-if="cuadrados">
+    <div v-for="fila in 3" :key="fila" class="tablero-fila">
+      <cuadrado v-for="i in 3" :key="indexPorFila(i, fila)"
+        :value="cuadrados[indexPorFila(i, fila)]"
+        :disabled="!!ganador"
+        :ganador="!!ganador && ganador.includes(indexPorFila(i, fila))"
+        @click="click(i, fila)" />
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Tablero',
+  props: {
+    cuadrados: Array,
+    ganador: Array
+  },
+  components: {
+    Cuadrado: () => import('./Cuadrado')
+  },
+  methods: {
+    indexPorFila (index, fila, max = 3) {
+      return (fila * max + index) - (max + 1)
+    },
+
+    click (index, fila) {
+      this.$emit('click', this.indexPorFila(index, fila));
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+.tablero {
+  background-color: aqua;
+}
+
+.tablero-fila {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: 1fr;
+}
+</style>
